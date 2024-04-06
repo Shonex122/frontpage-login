958e1a37029a3e3b321c5923d3618543dc310860
Mosh Hamedani <omoshola808@gmail.com>
2024-04-06 14:47:11 +0100

    changed the icon

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-// let password = document.getElementById("password");
-// let eyeicon = document.getElementById("eyeicon");
-// let icon = document.querySelector("img");
-// // let eyeIcon = document.getElementById("eyeIcon");
+let password = document.getElementById("password");
+let eyeicon = document.getElementById("eyeicon");
+let icon = document.querySelector("img");
+// let eyeIcon = document.getElementById("eyeIcon");
 
-// function showPassword() {
-//   if (password.type == "password") {
-//     password.type = "text";
-//     eyeicon.src = "eye-closed.png";
-//   } else {
-//     password.type = "password";
-//     eyeicon.src = "eye-open.png";
-//   }
-// }
+function showPassword() {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon.src = "eye-closed.png";
+  } else {
+    password.type = "password";
+    eyeicon.src = "eye-open.png";
+  }
+}
 
-const showPasswordDiv = document.getElementById("showPassword");
-const img_icon = showPassword.querySelector("img");
-const input = document.querySelector("#password");
+// const showPassword = document.getElementById("showPassword");
+// const img_icon = showPassword.querySelector("img");
+// const input = document.querySelector("#password");
 
-showPassword.addEventListener("click", () => {
-  input.type = input.type === "password" ? "text" : "password";
-  img_icon.src = img_icon.src.includes("open")
-    ? "eye-closed.png"
-    : "eye-closed.png";
-});
+// showPassword.addEventListener("click", () => {
+//   input.type = input.type === "password" ? "text" : "password";
+//   img_icon.src = img_icon.src.includes("open")
+//     ? "eye-closed.png"
+//     : "eye-closed.png";
+// });
